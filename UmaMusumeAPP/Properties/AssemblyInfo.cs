7bd269a5307f7a5522a4d1f40d2e745750202731
Kamijouko <MikuVick7139@gmail.com>
2021-03-24 17:39:59 +0900

    0.9.0.1

--- UmaMusumeAPP/Properties/AssemblyInfo.cs
+++ UmaMusumeAPP/Properties/AssemblyInfo.cs
@@ -6,7 +6,7 @@ using System.Runtime.InteropServices;
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
 [assembly: AssemblyTitle("UmaMusumeAPP")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyDescription("马娘相性计算器")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("UmaMusumeAPP")]
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 //可以指定所有这些值，也可以使用“生成号”和“修订号”的默认值
 //通过使用 "*"，如下所示:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("0.9.0.1")]
+[assembly: AssemblyFileVersion("0.9.0.0")]
